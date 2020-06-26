4317d3c4786fdb3e12a9584f1ba141eca1ebc919
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-06-27 00:00:53 +0530

    sample code

---
+++ Learn Python Books/ Automate the Boring Stuff with Python/sample.py
@@ -0,0 +1,11 @@
+print("hello")
+print("WORLD")
+
+print("hello",end='')
+print("WORLD")
+
+print('cats', 'dogs', 'mice')
+
+print('cats', 'dogs', 'mice',sep=",")
+
+# Excerpt From: Al Sweigart. “Automate the Boring Stuff with Python”. Apple Books.
